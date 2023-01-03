159ba414d7c95bca58239e0b1c7669baa2971275
BuguserOne <jinlingone@gmail.com>
2023-01-03 23:00:12 +0100

    Lev1_1_js-einführung_console-log

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/* a = 12;
+b = a * 3;
+console.log(b);
+document.write(b)
+
+console.log(document.URL); */
+
+let firstname = "Anton";
+console.log(firstname);
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(firstname + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet: " + true);
+document.write(firstname + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet: " + true); 
+
+
+
+/* let firstname;
+firsname = "Anton";
+console.log(firsname);
+
+let age;
+age = 28;
+console.log(age)
+
+let job;
+job = "Trainer"
+console.log(job)
+
+let married;
+married = true;
+console.log(married);
+
+console.log (firsname + " ist " + age + " Jahre alt,von Beruf " + job + " und ist verheiratet: " + true ) */
